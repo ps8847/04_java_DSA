7d5230b9754da9138f2856be3c14ba0ec8d7e2cf
Prince Sharma <ps789650@gmail.com>
2023-10-08 19:10:06 +0530

    added binary search basic questions

---
+++ 011_Binary_search_interview_questions/_01_ceiling_of_a_number.java
@@ -0,0 +1,36 @@
+
+public class _01_ceiling_of_a_number {
+    public static void main(String[] args) {
+        int[] arr = {-18,-12,-4,0,2,3,4,15,17,22,45,89};
+
+        int ans = ceiling(arr, 888);
+        System.out.println(ans);
+    }
+
+    static int ceiling(int[] arr , int target){
+
+        // if the target is greater than the greater number in the array
+
+        if(target > arr[arr.length - 1]){
+            return -1; // No ceiling found in this case
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            } else if(target > arr[mid]){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+       }
+
+}
